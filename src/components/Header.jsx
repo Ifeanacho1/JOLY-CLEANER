dda8d6312b03bef453288ee7efcab3a7a2a902f1
Ifeanacho1 <francisifeanacho1.com>
2026-05-20 18:18:39 +0100

    updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css'
+import Image from '../assets/Basket Logo.png'
+
+const Header = () => {
+  return (
+    <>
+      <header className='HeaderContainer'>
+        <div className='HeaderContainerLeft'>
+            <img src={Image} alt="" className='Logo'/>
+            <article className='LogoNameContainer'>
+                <h1 className='LogoName'>JOLY</h1>
+                <h2 className='LogoCleanerName'>Cleaners</h2>
+            </article>
+        </div>
+        <div className='HeaderContainerCenter'>
+            <h1 className='HeaderContainerCenterH1'>HOME</h1>
+            <h2 className='HeaderContainerCenterH2'>ABOUT</h2>
+            <h3 className='HeaderContainerCenterH3'>SERVICES</h3>
+        </div>
+        <div className='HeaderContainerRight'>
+            <button className='HeaderContainerRightButton'>BOOK A PICKUP</button>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
