1e5079c98faaf380cd0c2198017e17e431c01407
emmanueljohn-creatorl <emmanueljohnzion@gmail.com>
2026-05-21 04:28:52 -0700

    adding the service page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Hero.css"
+const Hero = () => {
+  return (
+    <>
+      <section className='service_layout'>
+      <article className='service_left'>
+        <div className='hero_info'>
+            <h3 className='lundery'> Our Laundry Service</h3>
+            <p className='lundery-2'>Professional Laundry Care designed to save you <br />
+            time and keep your clothes clean.</p>
+        </div>
+        </article> 
+        <article className='service_right'>
+            
+            </article>     
+      </section>
+    </>
+  )
+}
+
+export default Hero
